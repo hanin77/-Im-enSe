07e1704ea453aee7d80b4ffd74db7d5880d885cf
hanin77 <mohammadhanin77@gmail.com>
2023-01-27 11:44:07 +0000

    feat: add page layout

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-import 'bootstrap'
-import './sass/main.scss'
+import "bootstrap";
+import "./sass/main.scss";
 
+$(document).ready(function () {
+  $("#sidebarCollapse").on("click", function () {
+    $(".sidebar").toggleClass("active");
+  });
+});
