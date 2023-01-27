78a8a969c9833bb7b7c2e06f2a13bd9246837c0c
hanin77 <mohammadhanin77@gmail.com>
2023-01-27 17:26:56 +0000

    feat: add sidebar styling

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import "./sass/main.scss";
 $(document).ready(function () {
   $("#sidebarCollapse").on("click", function () {
     $(".sidebar").toggleClass("active");
+    $(".sidebar__header").toggleClass("crop");
   });
 });
