90a63c7817d7caf07f74f246d10bebaf3a9a2eec
hanin77 <mohammadhanin77@gmail.com>
2023-01-28 09:11:01 +0000

    fix: fix content width relative to sidebar width

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./sass/main.scss";
 $(document).ready(function () {
   $("#sidebarCollapse").on("click", function () {
     $(".sidebar").toggleClass("active");
+    $(".content").toggleClass("active");
     $(".sidebar__header").toggleClass("crop");
   });
 });
